83bfc5f898d9d9d85ec8b13145eb9907952c5822
lioveni99@gmail <root@ceph-1.com>
2016-03-25 10:33:29 +0800

    save

--- manifests/ceph/osd.pp
+++ manifests/ceph/osd.pp
@@ -10,27 +10,8 @@ class sunfire::ceph::osd (
   $ensure                 = present,
   $cluster                = 'ceph',
   $osd_device_dict        = {},
-#  $fsid                   = '066F558C-6789-4A93-AAF1-5AF1BA01A3AD',
-#  $mon_initial_members    = 'ceph-1,ceph-2,ceph-3',
-#  $mon_hosts              = '127.0.0.1',
-#  $bootstrap_osd_key      = 'AQABsWZSgEDmJhAAkAGSOOAJwrMHrM5Pz5On1A==',
   $osd_journal_size       = '10240',
-#  $mon_osd_full_ratio     = '0.95',
-#  $mon_osd_nearfull_ratio = '0.85',
   ){
-
-  #configure package repository
- # class { 'ceph::repo': }
-
-  # install and configure ceph
-#  class { 'ceph':
-#    fsid                   => $fsid,
-#    osd_journal_size       => $osd_journa_size,
-#    mon_osd_full_ratio     => $mon_osd_full_ratio,
-#    mon_osd_nearfull_ratio => $mon_osd_nearfull_ratio,
-#    mon_initial_members    => $mon_initial_members,
-#    mon_host               => $mon_hosts,
-#  }
 
   # initializing osd
   $osd_data_device_list = keys($osd_device_dict)
@@ -52,8 +33,4 @@ class sunfire::ceph::osd (
     } 
   }
   
-#  ceph::key {'client.bootstrap-osd':
-#    keyring_path => '/var/lib/ceph/bootstrap-osd/ceph.keyring',
-#    secret       => $bootstrap_osd_key,
-#  }
 }
--- manifests/ceph/rgw.pp
+++ manifests/ceph/rgw.pp
@@ -1,16 +1,9 @@
 class sunfire::ceph::rgw (
-#  $pkg_radosgw        = $::ceph::params::pkg_radosgw,
   $rgw_ensure                   = 'running',
   $rgw_enable                   = true,
   $rgw_name                     = $::hostname,
-#  $rgw_data           = "/var/lib/ceph/radosgw/ceph-${name}",
   $user                         = root,
-#  $keyring_path       = "/etc/ceph/ceph.client.${name}.keyring",
-#  $log_file           = '/var/log/ceph/radosgw.log',
   $rgw_dns_name                 = $::fqdn,
-#  $rgw_socket_path    = $::ceph::params::rgw_socket_path,
-#  $rgw_print_continue = false,
-#  $rgw_port           = undef,
   $frontend_type                = 'civetweb',
   $rgw_frontends                = "civetweb port=7480",
   $rgw_enable_apis              = "s3, admin",
@@ -18,7 +11,6 @@ class sunfire::ceph::rgw (
   $rgw_keystone_url             = "http://keyston.com:35357/",
   $rgw_keystone_admin_token     = "admin",
   $rgw_keystone_accepted_roles  = "_member_, admin",
-#  $syslog             = true,
   ) {
     # install and configure rgw
     include ::sunfire::ceph::params
@@ -37,17 +29,10 @@ class sunfire::ceph::rgw (
 
     ceph_config {
       "client.radosgw.${rgw_name}/admin_socket": value => $::sunfire::ceph::params::rgw_admin_socket;
-#      "client.radosgw.${name}/host": value => $::hostname;
-#      "client.radosgw.${name}/user": value => $user;
-#      "client.radosgw.${name}/keyring": value => $keyring_path;
-#      "client.radosgw.${name}/log_file": value => $rgw_log_file;
       "client.radosgw.${rgw_name}/rgw_dns_name": value => $rgw_dns_name;
-#      "client.radosgw.${name}/rgw_socket_path": value => $rgw_socket_path;
       "client.radosgw.${rgw_name}/rgw_s3_auth_use_keystone": value => $rgw_s3_auth_use_keystone;
-#      "client.radosgw.${name}/rgw_frontends": value => $rgw_frontends;
-      "client.radosgw.${rgw_name}/rgw_enable_apis": value => $rgw_enable_apis; #$::sunfire::ceph::params::rgw_enable_apias;
+      "client.radosgw.${rgw_name}/rgw_enable_apis": value => $rgw_enable_apis;
       "client.radosgw.${rgw_name}/rgw_enable_usage_log": value => $::sunfire::ceph::params::rgw_enable_usage_log;
-#      "client.radosgw.${name}/rgw_print_continue": value => $::sunfire::ceph::params::rgw_print_continue;
       "client.radosgw.${rgw_name}/rgw_override_bucket_index_max_shards": value => $::sunfire::ceph::params::rgw_override_bucket_index_max_shards;
       "client.radosgw.${rgw_name}/rgw_cache_enabled": value => $::sunfire::ceph::params::rgw_cache_enabled;
       "client.radosgw.${rgw_name}/rgw_cache_lru_size": value => $::sunfire::ceph::params::rgw_cache_lru_size;
